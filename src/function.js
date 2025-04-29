38e7682037910ec17073c5dba7cbed02a95ae1a2
Mufurun <ovis1gmelini@gmail.com>
2025-04-29 10:14:33 -0700

    files3

--- src/function.js
+++ src/function.js
@@ -1,4 +1,57 @@
 /*
+######################################################
+######     Functions for Inner HTML Control     ######
+######################################################
+
+    To maintain the folded sections:
+      updateParentHeight();
+      updateSibling();
+      updateChildren();
+    For the button of the list in each region
+      listMarkers()
+*/
+
+/*
+*********************************************
+******  Function updateParentHeight()  ******
+*********************************************
+
+  Adjust the size of parent section of the folded content
+    when you click, the parent section open and show the content without hiding by the other section
+*/
+
+export function updateParentHeight(element) {
+  let parent = element.parentElement;
+  while (parent) {
+    if (parent.classList.contains('content') && parent.style.maxHeight) {
+    parent.style.maxHeight = parent.scrollHeight  +" px";
+    }
+    parent = parent.parentElement;
+  }
+}
+
+/*
+****************************************
+******  Function updateSibling()  ******
+****************************************
+
+Unfold the other sibling sections
+This is necessary since the map will zoom up the region where you open the section with
+*/
+
+export function updateSibling(element) {
+    let sibling = element.parentElement.firstElementChild;
+    while (sibling) {
+        if (sibling !== element && sibling.classList.contains("content") && sibling.style.maxHeight) {
+            sibling.style.maxHeight = null;
+            sibling.previousElementSibling.classList.toggle("active");
+            updateChildren(sibling);
+        }
+        sibling = sibling.nextElementSibling;
+    }
+}
+
+/*
 *****************************************
 ******  Function updateChildren()  ******
 *****************************************
